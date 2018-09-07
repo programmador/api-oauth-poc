23f909bd79648432e743e27089615a75efeca257
Alexander Konotop <alexander.konotop@gmail.com>
2018-09-07 14:05:53 +0300

    Added proof of concept redis interaction and getting expire duration from config to the token storage service

--- src/Service/TokenStorage.php
+++ src/Service/TokenStorage.php
@@ -3,14 +3,23 @@
 namespace App\Service;
 
 use Psr\Log\LoggerInterface;
+use SymfonyBundles\RedisBundle\Redis\ClientInterface as Redis;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class TokenStorage
 {
+    private $container;
+    private $expires;
     private $logger;
+    private $redis;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, Redis $redis,
+        ContainerInterface $container)
     {
+        $this->container = $container;
+        $this->expires = $this->container->getParameter('tokens')['expires_in'];
         $this->logger = $logger;
+        $this->redis = $redis;
     }
 
     public function getToken()
@@ -21,6 +30,7 @@ class TokenStorage
             'Great work! Keep going!',
         ];
         $index = array_rand($messages);
+        $this->redis->set('library', $this->expires);
         $this->logger->info("hello from service " . $messages[$index]);
     }
 }
